6804739d06c26fdc5abe8e34f45e2d48ae06ad07
Kari <kari.lynn.dreyer@gmail.com>
2024-04-25 08:53:34 +0200

    Add "add item" and "cross out item" functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+function newItem() {
+
 //1. Adding a new item to the list of items: 
 let li = $('<li></li>')
 let inputValue = $('#input').val();
@@ -7,4 +9,15 @@ if (inputValue === '') {
     alert("You must write something!");
 } else {
     $('#list').append(li);
+}
+
+ //2. Crossing out an item from the list of items:
+ function crossOut() {
+    li.toggleClass('strike');
+ }
+
+ li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+ });
+
 }
